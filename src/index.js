939d3dc80596fd08ed53faa5f814bc4b4db0c8a3
Josh Mabry <mabry1985@gmail.com>
2020-01-09 14:22:53 -0800

    import clearuser action

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import firebase from './firebase';
 import rootReducer from './reducers/index';
 import Spinner from './Spinner';
 import 'semantic-ui-css/semantic.min.css'
-import { setUser } from './actions'
+import { setUser, clearUser } from './actions'
 
 import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom'
 
@@ -25,6 +25,9 @@ class Root extends React.Component {
       if(user){
         this.props.setUser(user)
         this.props.history.push("/")
+      } else {
+        this.props.history.push('/login');
+        this.props.clearUser();
       }
     })
   }
@@ -47,7 +50,7 @@ const mapStateToProps = state => ({
 const RootWithAuth = withRouter(
   connect(
     mapStateToProps, 
-    { setUser }
+    { setUser, clearUser }
   )(Root));
 
 ReactDOM.render(
