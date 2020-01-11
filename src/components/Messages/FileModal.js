5cbe5b393db506337c3fcd25e507ddb285bb50ae
Josh Mabry <mabry1985@gmail.com>
2020-01-11 15:26:59 -0800

    add functions to add, send, and clear file

--- src/components/Messages/FileModal.js
+++ src/components/Messages/FileModal.js
@@ -1,7 +1,37 @@
 import React, { Component } from 'react';
+import mime from 'mime-types';
 import { Modal, Input, Button, Icon } from 'semantic-ui-react';
 
 class FileModal extends Component {
+  state = {
+    file: null,
+    authorized: ['image/jpg', 'image/png']
+  }
+
+  addFile = event => {
+    const file = event.target.files[0];
+    if(file) {
+      this.setState({ file })
+    }
+  }
+
+  sendFile = () => {
+    const { file } = this.state;
+    const { uploadFile, closeModal } = this.props;
+    if(file !== null) {
+      if (this.isAuthorized(file.name)) {
+        const metadata = { contentType: mime.lookup(file.name) };
+        uploadFile(file, metadata);
+        closeModal();
+        this.clearFile();
+      }
+    }
+  }
+
+  clearFile = () => this.setState({ file: null });
+
+  isAuthorized = filename => this.state.authorized.includes(mime.lookup(filename));
+
   render() {
     const { modal, closeModal } = this.props;
 
@@ -10,6 +40,7 @@ class FileModal extends Component {
         <Modal.Header>Select an Image File</Modal.Header>
         <Modal.Content>
           <Input
+            onChange={this.addFile}
             fluid
             label="File types: jpg, png"
             name="file"
@@ -18,6 +49,7 @@ class FileModal extends Component {
         </Modal.Content>
         <Modal.Actions>
           <Button
+            onClick={this.sendFile}
             color="green"
             inverted
           >
