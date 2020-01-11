d0d4d58a3b39fd9b25934576c535b94f22b1e338
Josh Mabry <mabry1985@gmail.com>
2020-01-11 15:58:16 -0800

    add function and conditional render for image

--- src/components/Messages/Message.js
+++ src/components/Messages/Message.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import moment from 'moment';
-import { Comment } from 'semantic-ui-react';
+import { Comment, Image } from 'semantic-ui-react';
 
 const isOwnMessage = (message, user) => {
   return message.user.id === user.uid ? 'message__self' : '';
 }
 
 const timeFromNow = timestamp => moment(timestamp).fromNow();
+
+const isImage = (message) => {
+  return message.hasOwnProperty('image') && !message.hasOwnProperty('content');
+}
 
 const Message = ({ message, user }) => {
   return (
@@ -15,7 +19,9 @@ const Message = ({ message, user }) => {
       <Comment.Content className={isOwnMessage(message, user)}>
         <Comment.Author as="a">{message.user.name}</Comment.Author>
         <Comment.Metadata>{timeFromNow(message.timestamp)}</Comment.Metadata>
-        <Comment.Text>{message.content}</Comment.Text>
+        {isImage(message) ? 
+          <Image src={message.image} className="message__image"/> :
+          <Comment.Text>{message.content}</Comment.Text> }
       </Comment.Content>
     </Comment>
   )
